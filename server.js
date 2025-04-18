7e4be64bf3394a2b33b733f64811d113b8b5a747
vianey.jean <vianey.jean@ymail.com>
2025-04-18 22:41:11 +0400

    Mise a jour le Port à 10000

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 // Initialize express app
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
